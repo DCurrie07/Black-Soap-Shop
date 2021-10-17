8c838888a5cb3e6bacee43edeb956a39074c1592
DCurrie07 <demarcus.currie@yahoo.com>
2021-10-17 15:14:04 -0500

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Navbar from './Navbar';
+import ProductList from './ProductList';
+import Details from './Details';
+import Cart from './Cart';
+import Default from './Default';
+
+export default class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
+}
