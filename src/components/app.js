ae169a78d850afbc94b7e51353c2758150fa260a
DCurrie07 <demarcus.currie@yahoo.com>
2021-10-17 18:57:48 -0500

    added react bootstrap navbar

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Navbar from './Navbar';
+import Navbars from './Navbars';
 import ProductList from './ProductList';
 import Details from './Details';
 import Cart from './Cart';
 import Default from './Default';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar />
+        <Navbars />
         <Switch>
           <Route exact path="/" component={ProductList} />
           <Route path="/details" component={Details} />
