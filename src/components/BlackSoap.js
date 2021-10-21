d407206bc32e50077c0661cbdd7c24f2a5729cfd
DCurrie07 <demarcus.currie@yahoo.com>
2021-10-21 18:15:03 -0500

    adding app.py file

--- src/components/BlackSoap.js
+++ src/components/BlackSoap.js
@@ -1,35 +1,48 @@
 import React, { Component } from 'react';
 
 import { Card, Button } from 'react-bootstrap';
-import blacksoap from '../../images/black-soap.jpg'
+import blacksoap from '../../images/black-soap.jpg';
+import pic1 from '../../images/black-soap-bar.jpg';
 
 export default class BlackSoap extends Component {
     render() {
         return (
-            <div className="product-list">
-                <Card style={{ width: '30rem' }}>
-                    <Card.Img variant="top" src={blacksoap} />
-                    <Card.Body>
-                        <Card.Title>African Black Soap</Card.Title>
+            <div className="black-soap-product">
+                <div className="about-section">
+                    <Card>
+                        <Card.Img variant="top" src={pic1} />
+                        <Card.Body>
                         <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
+                            AFRICAN BLACK SOAP
                         </Card.Text>
-                        <Button variant="primary">Add to Cart</Button>
-                    </Card.Body>
+                        </Card.Body>
                     </Card>
+                </div>
+                <div className="product-list">
+                    <Card style={{ width: '30rem' }}>
+                        <Card.Img variant="top" src={blacksoap} />
+                        <Card.Body>
+                            <Card.Title>African Black Soap</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Add to Cart</Button>
+                        </Card.Body>
+                        </Card>
 
-                    <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={blacksoap} />
-                    <Card.Body>
-                        <Card.Title>African Black Soap</Card.Title>
-                        <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Add to Cart</Button>
-                    </Card.Body>
-                    </Card>
+                        <Card style={{ width: '30rem' }}>
+                        <Card.Img variant="top" src={blacksoap} />
+                        <Card.Body>
+                            <Card.Title>African Black Soap</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Add to Cart</Button>
+                        </Card.Body>
+                        </Card>
+                </div>
             </div>
         )
     }
